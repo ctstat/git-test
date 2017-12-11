2c80d56f03d54f5fad769594a91574a715eccf6f
Chunhao Tu <ctstat@outlook.com>
2017-12-11 09:34:31 -0500

    fix another bug in plt

--- test.py
+++ test.py
@@ -2,12 +2,10 @@
 import random
 a = [random.randint(10,30) for i in range(15)]
 b = [random.gauss(0,1) for i in range(15)]
+c = [i * j for i, j in zip(a, b)]
+
 
 import matplotlib.pyplot as plt
-plt.plot(x=a, y=b)
+plt.scatter(x=a, y=b)
 plt.show()
 
-# print c
-c = [i*j for i, j in zip(a, b)]
-print(c)
- 
