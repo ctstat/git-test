23148bab7f8542c2b3ff304707d3970b72fa32f0
Chunhao Tu <ctstat@outlook.com>
2017-12-11 14:56:58 -0500

    time function

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-# a new random file
-import random
-a = [random.randint(10,30) for i in range(15)]
-b = [random.gauss(0,1) for i in range(15)]
-c = [i * j for i, j in zip(a, b)]
-
-
-import matplotlib.pyplot as plt
-plt.scatter(x=a, y=b)
-plt.show()
-
+def time(x,y):
+    return(x*y)
