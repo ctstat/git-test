d3e98c85070353a76ccfff80e28f9f73337f0768
Chunhao Tu <ctu@une.edu>
2021-06-26 14:58:08 -0400

    square fun to cpp

--- c1.cpp
+++ c1.cpp
@@ -3,11 +3,16 @@
 
 using namespace std;
 
+
+double square(int x){
+    return x*x;
+}
+
 int main(){
     vector<int> nums;
 
     for(int i = 0; i < 10 ; i++){
-        nums.push_back(i + 100);
+        nums.push_back(square(i));
     }
 
     for(auto& i:nums){
