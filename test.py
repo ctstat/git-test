967755fccd3892514230cc92a6ce5d133da8310e
Chunhao Tu <ctu@une.edu>
2021-06-26 08:32:49 -0400

    change tst.py

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
 import numpy as np 
 import matplotlib.pyplot as plt   
 
-np.random.seed(12)
-
-x = list(range(11))
-
-a, b = 0.5, np.pi
-y = np.cos([(a + b*i) for i in x])
-
-plt.scatter(x, y, marker= "+")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
